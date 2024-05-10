2c1fc1662e95667ede830475d284902f21ef4e06
felipecarvalho02 <168842138+felipecarvalho02@users.noreply.github.com>
2024-05-10 09:26:21 -0300

    Add files via upload

---
+++ aula2_questao4.py
@@ -0,0 +1,11 @@
+juros = 1.01
+saldo = 500.0
+print(saldo)
+saldo = saldo * juros #505 redimentos1
+print(saldo)
+saldo = saldo * juros #rendimentos2
+print(saldo)
+saldo = saldo * juros #rendimenotos3
+print(saldo)
+
+print(type(saldo))
